31ea964d465faac648fde094b18558b4735db0e8
Richard <43622026+PixelMemories@users.noreply.github.com>
2024-04-08 20:36:00 -0400

    Update myshll.c

--- myshll.c
+++ myshll.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 
 char SHELL_NAME[50] = "myShell";
 int QUIT = 0;
