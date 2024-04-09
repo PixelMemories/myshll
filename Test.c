903642dc4ee7101dde8350c1dbe96dcc59a261e9
Richard <43622026+PixelMemories@users.noreply.github.com>
2024-04-08 22:50:25 -0400

    Update Test.c

--- Test.c
+++ Test.c
@@ -263,8 +263,7 @@ int myShell_execute(char **args) {
     if (redirect_input) {
         glob(input_file, GLOB_TILDE | GLOB_ERR, NULL, &glob_result);
         if (glob_result.gl_pathc > 0) {
-            input_file = glob_result.gl_pathv[0];
-            printf("in: %s\n", input_file);
+            input_file = strdup(glob_result.gl_pathv[0]); // Copying the expanded file path
         } else {
             printf("Wildcard pattern for input file does not match any files.\n");
             return 1;
@@ -276,8 +275,7 @@ int myShell_execute(char **args) {
     if (redirect_output) {
         glob(output_file, GLOB_TILDE | GLOB_ERR, NULL, &glob_result);
         if (glob_result.gl_pathc > 0) {
-            output_file = glob_result.gl_pathv[0];
-            printf("out: %s\n", output_file);
+            output_file = strdup(glob_result.gl_pathv[0]); // Copying the expanded file path
         } else {
             printf("Wildcard pattern for output file does not match any files.\n");
             return 1;
