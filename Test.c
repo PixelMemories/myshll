3c68bceb545803aa01ec9982f67a73a85dae6fce
Richard <43622026+PixelMemories@users.noreply.github.com>
2024-04-08 21:37:58 -0400

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,219 +1,382 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <fcntl.h>
-#include <glob.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define MAX_ARGS 64
-#define MAX_COMMANDS 64
-#define MAX_PATH 1024
 
-char *builtin_commands[] = {"cd", "pwd", "which", "exit"};
-char *builtin_paths[] = {"/bin/", "/usr/bin/", "/usr/local/bin/"};
+char SHELL_NAME[50] = "myShell";
+int QUIT = 0;
 
-void execute_command(char *command);
+#define MAX_COMMAND_LENGTH 1024
 
-int is_builtin(char *command) {
-    for (int i = 0; i < sizeof(builtin_commands) / sizeof(char *); i++) {
-        if (strcmp(command, builtin_commands[i]) == 0) {
-            return 1;
+// Function to read a line from command into the buffer
+char *readLine() {
+    char *line = (char *)malloc(sizeof(char) * 1024);
+    char c;
+    int pos = 0, bufsize = 1024;
+    if (!line) {
+        printf("\nBuffer Allocation Error.");
+        exit(EXIT_FAILURE);
+    }
+    while (1) {
+        c = getchar();
+        if (c == EOF || c == '\n') {
+            line[pos] = '\0';
+            return line;
+        } else {
+            line[pos] = c;
+        }
+        pos++;
+        if (pos >= bufsize) {
+            bufsize += 1024;
+            line = realloc(line, sizeof(char) * bufsize);
+            if (!line) {
+                printf("\nBuffer Allocation Error.");
+                exit(EXIT_FAILURE);
+            }
         }
     }
-    return 0;
 }
 
-void cd_command(char *args[]) {
+// Function to split a line into constituent commands
+char **splitLine(char *line) {
+    char **tokens = (char **)malloc(sizeof(char *) * 64);
+    char *token;
+    char delim[10] = " \t\n\r\a";
+    int pos = 0, bufsize = 64;
+    if (!tokens) {
+        printf("\nBuffer Allocation Error.");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, delim);
+    while (token != NULL) {
+        tokens[pos] = token;
+        pos++;
+        if (pos >= bufsize) {
+            bufsize += 64;
+            line = realloc(line, bufsize * sizeof(char *));
+            if (!line) {
+                printf("\nBuffer Allocation Error.");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, delim);
+    }
+    tokens[pos] = NULL;
+    return tokens;
+}
+
+
+// Function Declarations
+int myShell_cd(char **args);
+int myShell_exit();
+int myShell_redirect_input_output(char **args);
+int myShell_pwd();
+int myShell_which(char **args);
+
+
+// Definitions
+char *builtin_cmd[] = {"cd", "exit", "pwd", "which"};
+
+int (*builtin_func[])(char **) = {&myShell_cd, &myShell_exit, &myShell_pwd, &myShell_which};
+
+int numBuiltin() {
+    return sizeof(builtin_cmd) / sizeof(char *);
+}
+
+// Builtin command definitions
+int myShell_cd(char **args) {
     if (args[1] == NULL) {
-        fprintf(stderr, "Usage: cd <directory>\n");
+        printf("myShell: expected argument to \"cd\"\n");
     } else {
         if (chdir(args[1]) != 0) {
-            perror("chdir");
+            perror("myShell: ");
         }
     }
+    return 1;
 }
 
-void pwd_command() {
-    char cwd[MAX_PATH];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s\n", cwd);
+int myShell_exit() {
+    printf("Exiting Shell... See you soon!!\n");
+    QUIT = 1;
+    return 0;
+}
+
+int myShell_pwd(){
+    // dynamic array?????
+    char pwd[1024]; 
+
+    if (getcwd(pwd, sizeof(pwd)) != NULL) {
+        printf("%s\n", pwd);
     } else {
-        perror("getcwd");
-    }
-}
-
-void which_command(char *args[]) {
-    if (args[1] == NULL) {
-        fprintf(stderr, "Usage: which <command>\n");
-    } else {
-        char command_path[MAX_PATH];
-        int found = 0;
-        for (int i = 0; i < sizeof(builtin_paths) / sizeof(char *); i++) {
-            strcpy(command_path, builtin_paths[i]);
-            strcat(command_path, args[1]);
-            if (access(command_path, X_OK) == 0) {
-                printf("%s\n", command_path);
-                found = 1;
-                break;
-            }
-        }
-        if (!found) {
-            printf("%s: command not found\n", args[1]);
-        }
-    }
-}
-
-void exit_command(char *args[]) {
-    for (int i = 1; args[i] != NULL; i++) {
-        printf("%s ", args[i]);
-    }
-    printf("\n");
-    exit(EXIT_SUCCESS);
-}
-
-void execute_builtin_command(char *args[]) {
-    if (strcmp(args[0], "cd") == 0) {
-        cd_command(args);
-    } else if (strcmp(args[0], "pwd") == 0) {
-        pwd_command();
-    } else if (strcmp(args[0], "which") == 0) {
-        which_command(args);
-    } else if (strcmp(args[0], "exit") == 0) {
-        exit_command(args);
-    }
-}
-
-void tokenize_input(char *input, char *tokens[]) {
-    char *token;
-    int i = 0;
-
-    token = strtok(input, " \t\n");
-    while (token != NULL && i < MAX_ARGS - 1) {
-        tokens[i++] = token;
-        token = strtok(NULL, " \t\n");
-    }
-    tokens[i] = NULL;
-}
-
-void execute_command(char *command) {
-    char *commands[MAX_COMMANDS];
-    char *tokens[MAX_ARGS];
-    int i = 0;
-
-    tokenize_input(command, tokens);
-
-    commands[i] = tokens[0];
-    for (int j = 1; tokens[j] != NULL; j++) {
-        if (strcmp(tokens[j], "|") == 0) {
-            commands[++i] = NULL;
-        } else {
-            commands[i] = tokens[j];
-        }
-    }
-    commands[++i] = NULL;
-
-    if (commands[0] == NULL) {
-        return;
-    }
-
-    int pipes[MAX_COMMANDS - 1][2];
-    int fd_in = 0; // File descriptor for input
-
-    for (i = 0; commands[i] != NULL; i++) {
-        int j = 0;
-
-        for (j = 0; commands[j] != NULL; j++) {
-            glob_t globbuf;
-            memset(&globbuf, 0, sizeof(glob_t));
-            if (strchr(commands[j], '*') != NULL || strchr(commands[j], '?') != NULL) {
-                if (glob(commands[j], GLOB_NOCHECK | GLOB_TILDE, NULL, &globbuf) == 0) {
-                    for (int k = 0; k < globbuf.gl_pathc; k++) {
-                        commands[j] = globbuf.gl_pathv[k];
-                        execute_builtin_command(commands);
-                    }
-                    globfree(&globbuf);
-                }
-            }
-        }
-
-        if (pipe(pipes[i]) == -1) {
-            perror("pipe");
-            exit(EXIT_FAILURE);
-        }
-
-        pid_t pid = fork();
-        if (pid == 0) {
-            // Child process
-            if (i == 0) {
-                // First command
-                dup2(pipes[i][1], 1); // Redirect output to the write end of the first pipe
-            } else if (commands[i + 1] != NULL) {
-                // Intermediate command
-                dup2(fd_in, 0); // Redirect input from the read end of the previous pipe
-                dup2(pipes[i][1], 1); // Redirect output to the write end of the current pipe
-            } else {
-                // Last command
-                dup2(fd_in, 0); // Redirect input from the read end of the previous pipe
-                // Check for output redirection
-                for (j = 0; commands[j] != NULL; j++) {
-                    if (strcmp(commands[j], ">") == 0) {
-                        commands[j] = NULL;
-                        int fd_out = open(commands[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0640);
-                        if (fd_out == -1) {
-                            perror("open");
-                            exit(EXIT_FAILURE);
-                        }
-                        dup2(fd_out, 1); // Redirect output to the specified file
-                        close(fd_out);
-                    }
-                }
-            }
-            // Close all pipe ends
-            for (int k = 0; k < i; k++) {
-                close(pipes[k][0]);
-                close(pipes[k][1]);
-            }
-            execute_builtin_command(commands);
-            execvp(commands[0], commands);
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            // Error forking
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
-
-        close(pipes[i][1]); // Close write end of the pipe
-        fd_in = pipes[i][0]; // Set the input for the next command
-    }
-
-    for (int j = 0; j < i; j++) {
-        close(pipes[j][0]); // Close read end of the pipe
-        close(pipes[j][1]); // Close write end of the pipe
-    }
-
-    for (int j = 0; j < i; j++) {
-        wait(NULL); // Wait for all child processes to complete
-    }
-}
-
-int main() {
-    char command[MAX_PATH];
-
-    while (1) {
-        printf("$ ");
-        fflush(stdout);
-
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            break;
-        }
-
-        // Removing trailing newline character
-        command[strcspn(command, "\n")] = 0;
-
-        execute_command(command);
+        perror("pwd error");
+        return 1;
     }
 
     return 0;
+}
+
+int myShell_which(char **args) {
+    int argc = 0;
+
+    // Count the number of arguments
+    while (args[argc] != NULL) {
+        argc++;
+    }
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <program_name>\n", args[0]);
+        return 1; // Return error code 1 for wrong number of arguments
+    }
+
+    char *program_name = args[1];
+    
+    /*
+    if(strcmp(program_name, "exit") == 0 || strcmp(program_name, "cd") == 0 || strcmp(program_name, "pwd") == 0 || strcmp(program_name, "which") == 0) {
+        printf("Built-in Command does not have Path\n");
+        return 1; 
+    }
+    */
+    
+
+    char *path = getenv("PATH");
+
+    if (path == NULL) {
+        fprintf(stderr, "Error: PATH environment variable is not set.\n");
+        return 1;
+    }
+
+    char *path_copy = strdup(path);
+    if (path_copy == NULL) {
+        fprintf(stderr, "Error: Memory allocation failed.\n");
+        return 1;
+    }
+
+    char *token;
+    char file_path[1024];
+
+    token = strtok(path_copy, ":");
+    while (token != NULL) {
+        snprintf(file_path, 1024, "%s/%s", token, program_name);
+        if (access(file_path, X_OK) == 0) {
+            printf("%s\n", file_path);
+            free(path_copy);
+            return 0;
+        }
+        token = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+
+    fprintf(stderr, "%s: program not found\n", args[1]);
+    return 1;
+}
+
+
+void execute_command(const char *command) {
+    char *args[MAX_ARGS];
+    char *token, *rest;
+    int arg_count = 0;
+    int pipe_fd[2];
+    pid_t pid;
+
+    rest = strdup(command);
+
+    // Parse command into tokens
+    while ((token = strtok_r(rest, " ", &rest))) {
+        if (strcmp(token, "|") == 0) { // Handle pipe
+            args[arg_count] = NULL;
+            arg_count = 0;
+
+            pipe(pipe_fd);
+
+            if ((pid = fork()) == 0) { // Child process
+                close(pipe_fd[0]);
+                dup2(pipe_fd[1], STDOUT_FILENO);
+                close(pipe_fd[1]);
+                execvp(args[0], args);
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            } else if (pid < 0) { // Fork error
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else { // Parent process
+                close(pipe_fd[1]);
+                wait(NULL);
+                dup2(pipe_fd[0], STDIN_FILENO);
+                close(pipe_fd[0]);
+            }
+        } else if (strcmp(token, ">") == 0) { // Handle redirect output
+            token = strtok_r(NULL, " ", &rest);
+            int fd = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        } else if (strcmp(token, "<") == 0) { // Handle redirect input
+            token = strtok_r(NULL, " ", &rest);
+            int fd = open(token, O_RDONLY);
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        } else { // Handle command arguments
+            glob_t glob_result;
+            if (strchr(token, '*') != NULL) { // Check for wildcard
+                glob(token, GLOB_TILDE, NULL, &glob_result);
+                for (int i = 0; i < glob_result.gl_pathc; ++i) {
+                    args[arg_count++] = strdup(glob_result.gl_pathv[i]);
+                }
+                globfree(&glob_result);
+            } else {
+                args[arg_count++] = strdup(token);
+            }
+        }
+    }
+
+    args[arg_count] = NULL;
+
+    if ((pid = fork()) == 0) { // Child process
+        execvp(args[0], args);
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) { // Fork error
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else { // Parent process
+        wait(NULL);
+    }
+
+    free(rest);
+    for (int i = 0; i < arg_count; ++i) {
+        free(args[i]);
+    }
+}
+
+
+
+int myShellLaunch(char **args) {
+    pid_t pid, wpid;
+    int status;
+    pid = fork();
+    if (pid == 0) {
+        // The Child Process
+        if (execvp(args[0], args) == -1) {
+            perror("myShell: ");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    } else if (pid < 0) {
+        // Forking Error
+        perror("myShell: ");
+    } else {
+        // The Parent Process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+// Function to execute command from terminal
+int execShell(char **args) {
+    if (args[0] == NULL) {
+        return 1;
+    }
+    // Handle piping
+    if (myShell_pipe(args)) {
+        return 1; // Pipe handled
+    }
+    // Loop to check for builtin functions
+    for (int i = 0; i < numBuiltin(); i++) {
+        if (strcmp(args[0], builtin_cmd[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    // Handle redirection
+    if (myShell_redirect_input_output(args)) {
+        return 1;
+    }
+    // Handle Wildcards
+    if (expand_wildcards(args)){
+        return 1;
+    }
+    
+    // If no piping or redirection, launch command normally
+    return myShellLaunch(args);
+}
+
+// When myShell is called Interactively
+int myShellInteract() {
+    char *line;
+    char **args;
+    while (QUIT == 0) {
+        printf("%s> ", SHELL_NAME);
+        line = readLine();
+        execute_command(args);
+        free(line);
+        free(args);
+    }
+    return 1;
+}
+
+// When myShell is called with a Script as Argument
+int myShellBatch(FILE *filename) {
+    char line[MAX_COMMAND_LENGTH];
+    char **args;
+    if (filename == NULL) {
+        printf("\nUnable to open file.");
+        return 1;
+    } else {
+        printf("\nFile Opened. Parsing. Parsed commands displayed first.");
+        while (fgets(line, sizeof(line), filename) != NULL) {
+            printf("\n%s", line);
+            args = splitLine(line);
+            execShell(args);
+        }
+    }
+    free(args);
+    fclose(filename);
+    return 1;
+}
+
+int BMCheck(int argc, char *argv[]) {
+    // Check if there are command-line arguments
+    if (argc > 1) {
+        return 1; // Running in batch mode
+    }
+
+    // Check if input is being piped
+    if (!isatty(fileno(stdin))) {
+        return 1; // Running in batch mode
+    }
+
+    return 0; // Running in interactive mode
+}
+
+int main(int argc, char **argv) {
+    // Parsing commands Interactive mode or Script Mode
+    if (BMCheck(argc, argv)) {
+        if (argc > 1) {
+            printf("Running in batch mode with file: %s\n", argv[1]);
+            FILE *file = fopen(argv[1], "r");
+            if (file == NULL) {
+                perror("Error opening file");
+                return 1;
+            }
+            myShellBatch(file);
+        } else {
+            printf("Running in batch mode with piped input\n");
+            myShellBatch(stdin);
+        }
+
+    } else {
+        printf("Running in interactive mode\n");
+        printf("Welcome to your personal Shell!!\n");
+        myShellInteract();
+
+    }
+    
+    // Exit the Shell
+    return EXIT_SUCCESS;
+}
